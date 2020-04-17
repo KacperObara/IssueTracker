abc57f64ccebb1804966a3a7c3c506123ee100f9
Kodeman010 <obara.kacper0@gmail.com>
2020-04-17 22:01:34 +0200

    Synchronization

---
+++ IssueTrackerAPI/Models/Assignee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTrackerAPI.Models
+{
+    public class Assignee
+    {
+        public int IssueId { get; set; }
+        public int PersonId { get; set; }
+        public Issue Issue { get; set; }
+        public Person Person { get; set; }
+    }
+}
--- IssueTrackerAPI/Models/Project.cs
+++ IssueTrackerAPI/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace IssueTrackerAPI.Models
 {
     public class Project
     {
-        public int Id { get; set; }
+        [Key]
+        public int Id_Project { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreationDate { get; set; }
