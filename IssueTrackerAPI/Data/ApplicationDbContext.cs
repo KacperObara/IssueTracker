d776f683283bb9336bc7cfa6d99dc7bf790a790a
Kodeman010 <obara.kacper0@gmail.com>
2020-04-18 17:58:52 +0200

    Update ApplicationDbContext.cs

--- IssueTrackerAPI/Data/ApplicationDbContext.cs
+++ IssueTrackerAPI/Data/ApplicationDbContext.cs
@@ -13,6 +13,34 @@ namespace IssueTrackerAPI.Data
             : base(options)
         {
         }
-        public DbSet<IssueTrackerAPI.Models.Severity> Severity { get; set; }
+        public DbSet<Assignee> Assignees { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectMember> ProjectMembers { get; set; }
+        public DbSet<Severity> Severities { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Assignee>().ToTable(nameof(Assignee));
+            builder.Entity<Comment>().ToTable(nameof(Comment));
+            builder.Entity<Issue>().ToTable(nameof(Issue));
+            builder.Entity<Person>().ToTable(nameof(Person));
+            builder.Entity<Project>().ToTable(nameof(Project));
+            builder.Entity<ProjectMember>().ToTable(nameof(ProjectMember));
+            builder.Entity<Severity>().ToTable(nameof(Severity));
+            builder.Entity<Status>().ToTable(nameof(Status));
+
+            builder.Entity<Assignee>()
+                .HasKey(a => new { a.IssueId, a.PersonId });
+
+            builder.Entity<ProjectMember>()
+                .HasKey(pm => new { pm.ProjectId, pm.PersonId });
+
+            builder.Entity<Comment>()
+                .HasKey(c => new { c.IssueId, c.PersonId });
+        }
     }
 }
